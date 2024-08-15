1310a3e2ae4da94b64b5a30f4923c1bd2e51a48f
JakubDomarecki <kuba86952@wp.pl>
2024-08-16 00:05:01 +0200

    Testowanie asynchronicznych zdarzeń w interfejsie użytkownika

---
+++ Testowanie asynchronicznych zdarzeń w interfejsie użytkownika/src/UserProfile.spec.jsx
@@ -0,0 +1,29 @@
+import {render, screen} from '@testing-library/react'
+import { UserProfile } from './UserProfile'
+
+describe('<UserProfile/>', () => {
+
+  test("displays loading state initially", async () => {
+    render(<UserProfile userId={3} />);
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+  
+  test('shows user data after successful loading', async () => {
+    render(<UserProfile userId={3}/>)
+
+    expect(await screen.findByText("John Doe")).toBeInTheDocument()
+    expect(screen.queryByText("Loading...")).not.toBeInTheDocument()
+  })
+
+  
+  test('handles user not found state', async () => {
+    render(<UserProfile userId={2}/>)
+    expect(await screen.findByText("User not found.")).toBeInTheDocument();
+  })
+
+  test("handles error state", async () => {
+    render(<UserProfile userId={1} />);
+    expect(await screen.findByText("Oops, something went wrong.")).toBeInTheDocument();
+  });
+
+})
